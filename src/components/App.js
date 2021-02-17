9b0433fba4e0d890b6a367b10d3469682a344d01
Ingrida <ingrida.surname@gmail.com>
2021-02-17 15:42:24 +0300

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import './index.css';
-// import logo from './images/logo__header.svg';
-import PopupWithForm from './components/PopupWithForm';
-import ImagePopup from './components/ImagePopup';
+import '../index.css';
+import PopupWithForm from './PopupWithForm';
+import ImagePopup from './ImagePopup';
 
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
 
